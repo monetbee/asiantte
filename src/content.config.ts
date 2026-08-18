00edba58503282ce4dc0ec7f86c0faf536d15f7f
monetbee <150294707+monetbee@users.noreply.github.com>
2026-08-18 16:38:26 +0900

    blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,19 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const pandoraBlog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/pandoraBlog/` directory.
+	loader: glob({ base: "./src/content/pandoraBlog", pattern: "**/*.{md,mdx}" }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		category: z.string().optional(),
+		tags: z.array(z.string()).optional(),
+	}),
+});
+
+export const collections = { blog, pandoraBlog };
